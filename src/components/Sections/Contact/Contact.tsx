4420ba15765b88e68cb6b8ce83bb5e9c930a777e
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-03-20 21:55:14 +0100

    REFACTOR - Contact section

---
+++ src/components/Sections/Contact/Contact.tsx
@@ -0,0 +1,19 @@
+export default function Contact() {
+  return (
+    <section className="w-full py-12 md:py-24">
+      <div className="container grid items-center gap-10 px-4 md:px-6 lg:grid-cols-2 lg:gap-16">
+        <div className="space-y-4">
+          <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">Contact Us</h2>
+          <p className="max-w-[600px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
+            Ready to experience the best plumbing services in town? Contact us to schedule an appointment or request
+            a quote.
+          </p>
+        </div>
+        <div className="mx-auto w-full max-w-sm space-y-2">
+          <form className="space-y-4">
+          </form>
+        </div>
+      </div>
+    </section>
+  )
+}
