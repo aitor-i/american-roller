76486a2438d76e778cf86e72963622fdef3bf3e8
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-03-21 19:28:02 +0100

    UPDATE - Header styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,13 +3,13 @@ import { PhoneCallIcon, WrenchIcon } from '../Icons/Icons'
 
 export default function Header() {
   return (
-    <header className="flex items-center justify-center h-24 gap-4 px-4 text-sm md:gap-6 lg:px-6">
+    <header className="flex bg-blue-500 items-center text-gray-50  justify-center h-24 gap-4 px-4 text-sm md:gap-6 lg:px-6">
       <div className="flex items-center gap-2">
         <WrenchIcon className="h-6 w-6" />
-        <span className="font-semibold">Plumbing Services</span>
+        <span className="font-semibold md:text-xl">Plumbing Services</span>
       </div>
       <div className="ml-auto flex items-center gap-2">
-        <PhoneCallIcon className="h-4 w-4 opacity-50" />
+        <PhoneCallIcon className="h-4 w-4 " />
         <span className="hidden md:inline">Call us: </span>
         <span className="font-semibold">1-800-123-4567</span>
       </div>
