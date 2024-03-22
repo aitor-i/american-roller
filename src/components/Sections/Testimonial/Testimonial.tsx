650b484aa0f1951eea06fa6a393f4e29d37b6f83
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-03-22 20:53:16 +0100

    UPDATE - Testimonials styles

--- src/components/Sections/Testimonial/Testimonial.tsx
+++ src/components/Sections/Testimonial/Testimonial.tsx
@@ -16,7 +16,7 @@ const dict = {
 
 export default function Testimonial() {
   return (
-    <section className="w-full text-blue-500 bg-white py-12 md:py-44">
+    <section className="w-full text-green-600 bg-white py-12 md:py-44">
       <div className="container grid items-center gap-10 px-4 md:px-6 lg:grid-cols-2 lg:gap-16">
         <div className="space-y-4">
           <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">{dict.title}</h2>
