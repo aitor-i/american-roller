69b8bb16ea8edb6b416686b25333b0e07ce983ca
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-03-22 20:54:23 +0100

    UPDATE -Footer styles

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 
 export default function Footer() {
   return (
-    <footer className="flex bg-blue-500 text-white font-bold flex-col gap-2 py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
+    <footer className="flex bg-yellow-400 text-gray-950 font-bold flex-col gap-2 py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
       <p className="text-xs dark:text-gray-400">© 2024 Acme Inc. All rights reserved.</p>
       <nav className="sm:ml-auto flex gap-4 sm:gap-6">
         <Link className="text-xs hover:underline underline-offset-4" href="#">
