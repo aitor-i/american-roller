76a6d51e73664f8ceda453e7208eea862c1721d2
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-03-22 20:53:52 +0100

    UPDATE - Contact styles

--- src/components/Sections/Contact/Contact.tsx
+++ src/components/Sections/Contact/Contact.tsx
@@ -9,7 +9,7 @@ const dict = {
 
 export default function Contact() {
   return (
-    <section className="w-full bg-blue-50 py-12 md:py-24">
+    <section className="w-full bg-yellow-50 py-12 md:py-24">
       <div className="container grid items-center gap-10 px-4 md:px-6 lg:grid-cols-2 lg:gap-16">
         <div className="space-y-4">
           <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">{dict.title}</h2>
