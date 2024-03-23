c1d10d41c766e2175146a2c0265561070987beb4
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-03-23 13:37:07 +0100

    UPDATE - Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,16 +1,9 @@
-import Link from "next/link"
 
 export default function Footer() {
   return (
     <footer className="flex bg-yellow-400 text-gray-950 font-bold flex-col gap-2 py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
-      <p className="text-xs dark:text-gray-400">© 2024 Acme Inc. All rights reserved.</p>
+      <p className="text-xs dark:text-gray-400">© 2024 Irati Electricidad Sl. Todos los derechos reservados.</p>
       <nav className="sm:ml-auto flex gap-4 sm:gap-6">
-        <Link className="text-xs hover:underline underline-offset-4" href="#">
-          Terms of Service
-        </Link>
-        <Link className="text-xs hover:underline underline-offset-4" href="#">
-          Privacy
-        </Link>
       </nav>
     </footer>
   )
