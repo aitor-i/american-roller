4d371e2d32628ebed67145e4ef2879f9733bafc9
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-03-27 22:26:09 +0100

    UPDATE - Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,16 @@
 import React from 'react'
 import Link from 'next/link'
-import { LightningIcon, PhoneCallIcon } from '../Icons/Icons'
+import { SunIcon, PhoneCallIcon, InstagramIcon } from '../Icons/Icons'
 
 export default function Header() {
   return (
-    <header className="flex w-full fixed bg-yellow-400 items-center text-gray-950  justify-center h-24 gap-4 px-4 text-sm md:gap-6 lg:px-6">
+    <header className="flex w-full fixed bg-red-500 text-gray-50 items-center font-bold justify-center h-32 gap-4 px-4 text-sm md:gap-6 lg:px-6">
       <div className="flex items-center gap-2">
-        <LightningIcon className="h-6 w-6" />
-        <span className="font-semibold md:text-xl">Irati Electricidad</span>
+        <span className="font-semibold md:text-2xl lg:text-3xl">American Roller</span>
       </div>
       <div className="ml-auto flex items-center gap-2">
-        <PhoneCallIcon className="h-4 w-4 " />
-        <span className="hidden md:inline">LLamanos: </span>
-        <Link href={"tel:+34948257429"} className="font-semibold">948 25 74 29</Link>
+        <InstagramIcon className="h-4 w-4 text-gray-50 " />
+        <Link href={"tel:+34948257429"} className="font-semibold">@american.roller</Link>
       </div>
     </header>
   )
