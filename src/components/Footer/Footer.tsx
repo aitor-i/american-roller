3b735c5568726e146a6e6953490e7c9d35e4ab8a
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-03-28 17:09:18 +0100

    UPDATE - Footer styles

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,8 +1,8 @@
 
 export default function Footer() {
   return (
-    <footer className="flex bg-yellow-400 text-gray-950 font-bold flex-col gap-2 py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
-      <p className="text-xs ">© 2024 Irati Electricidad Sl. Todos los derechos reservados.</p>
+    <footer className="flex bg-red-500 text-gray-50 font-bold flex-col gap-2 py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
+      <p className="text-xs font-light ">© 2024 Pasteleria American Roller Sl. Todos los derechos reservados.</p>
       <nav className="sm:ml-auto flex gap-4 sm:gap-6">
       </nav>
     </footer>
