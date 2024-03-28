5daaa422e64636c17253954b5675634d332ad134
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-03-28 16:59:48 +0100

    ADD - landing section

---
+++ src/components/Sections/Landing/Landing.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image"
+import image from "@/../public/DALL·E 2024-03-28 14.54.34 - A charming bakery scene in widescreen aspect ratio, vividly adorned with red, white, and blue. The facade of the bakery is painted in these patriotic .webp"
+
+const dict = {
+  titulo: "Pastelería American Roller",
+  subtitle: "Pastelería americana casera | Próxima apertura en Pamplona"
+}
+
+export default function Landing() {
+  return (
+    <header className="relative h-screen">
+      <Image
+        alt="Freshly baked goods"
+        className="absolute inset-0 object-cover w-full h-full blur-sm"
+        height="600"
+        src={image}
+        style={{
+          aspectRatio: "1200/600",
+          objectFit: "cover",
+        }}
+        width="1200"
+      />
+      <div className="relative z-10 flex flex-col items-center pt-40 h-full px-4 text-center text-white photo-overlay ">
+        <h1 className="text-4xl font-bold sm:text-5xl lg:text-8xl opacity-100 text-white">{dict.titulo}</h1>
+        <p className="mt-4 text-lg sm:text-xl lg:text-2xl max-w-2xl opacity-100">
+          {dict.subtitle}
+        </p>
+      </div>
+    </header>
+  )
+}
