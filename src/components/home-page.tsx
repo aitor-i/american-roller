8fdaff0bf0f8667f519117a4a874725a33d89da6
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-03-28 17:00:46 +0100

    UPDATE -Home page

--- src/components/home-page.tsx
+++ src/components/home-page.tsx
@@ -5,15 +5,18 @@ import Testimonial from "./Sections/Testimonial/Testimonial"
 import Contact from "./Sections/Contact/Contact"
 import Footer from "./Footer/Footer"
 import Map from "./Sections/MapSection/Map"
+import Landing from "./Sections/Landing/Landing"
+import Specialtis from "./Sections/Specialties/Specialtis"
+import Opinions from "./Sections/Opinions/Opinions"
 
 export function HomePage() {
   return (
     <div className="flex flex-col min-h-[100dvh]">
       <main className="flex-1">
         <Header />
-        <Presentation />
-        <Services />
-        <Testimonial />
+        <Landing />
+        <Specialtis />
+        <Opinions />
         <Contact />
         <Map />
       </main>
