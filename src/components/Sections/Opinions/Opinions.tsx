c92603bedcd0da1876bcf7d75a620f0493cdd924
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-03-28 17:09:39 +0100

    REFACTOR - Opinions

--- src/components/Sections/Opinions/Opinions.tsx
+++ src/components/Sections/Opinions/Opinions.tsx
@@ -1,24 +1,12 @@
-import Image from "next/image"
+import Opinion from "./Opinion/Opinion";
+
 export default function Opinions() {
   return (
-    <section className="py-12 px-4 bg-white md:py-40 ">
+    <section className="py-12 flex flex-col gap-8 px-4 bg-white md:py-40 ">
       <h2 className="text-3xl text-blue-600 font-bold text-center sm:text-4xl lg:text-5xl">What Our Customers Say</h2>
       <div className="grid gap-6 mt-8 sm:grid-cols-2 lg:grid-cols-3">
-        <blockquote className="flex flex-col items-center text-center">
-          <Image
-            alt="Customer"
-            className="w-24 h-24 rounded-full object-cover"
-            height="100"
-            src="/placeholder.svg"
-            style={{
-              aspectRatio: "100/100",
-              objectFit: "cover",
-            }}
-            width="100"
-          />
-          <p className="mt-4 font-light text-blue-500">"The best homemade bakery in town! Their bread is my absolute favorite."</p>
-          <cite className="mt-2 text-blue-500">- Jane Doe</cite>
-        </blockquote>
+        <Opinion />
+        <Opinion />
       </div>
     </section>
   )
