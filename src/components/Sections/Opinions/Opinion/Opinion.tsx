d18c0bcccdeb78e4a0f8218fffcd4db3b6a35f54
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-03-29 11:50:31 +0100

    ADD - Dict to opinion

--- src/components/Sections/Opinions/Opinion/Opinion.tsx
+++ src/components/Sections/Opinions/Opinion/Opinion.tsx
@@ -1,6 +1,11 @@
 import Image from "next/image"
 import jane from "@/../public/jane-doe.webp"
 
+const dict = {
+  opinion: "The best homemade bakery in town! Their bread is my absolute favorite.",
+  client: "- Jane Doe"
+}
+
 export default function Opinion() {
   return (
     <blockquote className="flex flex-col items-center text-center">
@@ -15,8 +20,8 @@ export default function Opinion() {
         }}
         width="150"
       />
-      <p className="mt-4 font-light text-blue-500">"The best homemade bakery in town! Their bread is my absolute favorite."</p>
-      <cite className="mt-2 text-blue-500">- Jane Doe</cite>
+      <p className="mt-4 font-light text-blue-500">{dict.opinion}</p>
+      <cite className="mt-2 text-blue-500">{dict.client}</cite>
     </blockquote>
   )
 }
